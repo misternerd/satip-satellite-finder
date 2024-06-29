728c78ef2c686637a8f1810906b180a2e74225a1
Jan <misternerd@users.noreply.github.com>
2024-06-29 06:30:19 +0000

    Fixes for Display
    * Get list of tuner names instead of showing only index
    * Added a lock, so multiple threads can write to display
    * Added dedicated start method
    * When closing display, make sure it has started in the first place

--- src/display.py
+++ src/display.py
@@ -6,6 +6,8 @@ from rich.live import Live
 from rich.panel import Panel
 from rich.progress import Progress, BarColumn, TextColumn, TaskID, TaskProgressColumn
 
+from threading import Lock
+
 
 @dataclass
 class TunerData:
@@ -16,13 +18,14 @@ class TunerData:
 
 
 class Display:
+    _lock = Lock()
     _tuners: list[TunerData] = []
     _console = Console()
 
-    def __init__(self, number_of_tuners: int):
+    def __init__(self, tuner_display_names: list[str]):
         rows = []
-        self._layout = Layout(size=number_of_tuners * 2)
-        for i in range(number_of_tuners):
+        self._layout = Layout(size=len(tuner_display_names) * 2)
+        for i, display_name in enumerate(tuner_display_names):
             level_progress = Progress(TextColumn("Signal"),
                                       BarColumn(),
                                       TaskProgressColumn(),
@@ -39,25 +42,29 @@ class Display:
             snr_task = snr_progress.add_task("SNR", total=100)
             row_layout = Layout(name=f'Tuner {i + 1}', size=2)
             row_layout.split_row(level_progress, snr_progress)
-            panel = Panel(row_layout, title=f'Tuner {i + 1}', border_style='blue')
+            panel = Panel(row_layout, title=f'Tuner {i + 1} ({display_name})', border_style='blue')
             rows.append(panel)
             self._tuners.append(TunerData(level_progress, level_task, snr_progress, snr_task))
         self._layout.split_column(*rows)
         self._live = Live(self._layout, console=self._console, refresh_per_second=4)
+
+    def start(self):
         self._live.start(True)
 
     def close(self):
-        self._live.stop()
-        self._console.clear()
+        if self._live.is_started:
+            self._live.stop()
+            self._console.clear()
 
     def update_tuner(self, tuner_index: int, level: int, snr: int):
-        if tuner_index >= len(self._tuners):
-            raise IndexError(f'Invalid tuner index {tuner_index}')
+        with self._lock:
+            if tuner_index >= len(self._tuners):
+                raise IndexError(f'Invalid tuner index {tuner_index}')
 
-        # this can happen during shutdown
-        if not self._live.is_started:
-            return
+            # this can happen during shutdown
+            if not self._live.is_started:
+                return
 
-        tuner_data = self._tuners[tuner_index]
-        tuner_data.level_progress.update(tuner_data.level_task, completed=level)
-        tuner_data.snr_progress.update(tuner_data.snr_task, completed=snr)
+            tuner_data = self._tuners[tuner_index]
+            tuner_data.level_progress.update(tuner_data.level_task, completed=level)
+            tuner_data.snr_progress.update(tuner_data.snr_task, completed=snr)
